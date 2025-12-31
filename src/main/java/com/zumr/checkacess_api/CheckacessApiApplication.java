91e33d08bbc9cf4bf67fec29fdcbe8b0a49b729c
Erick Daniel <derickanielgg@gmail.com>
2025-12-31 16:10:16 -0300

    project created

---
+++ src/main/java/com/zumr/checkacess_api/CheckacessApiApplication.java
@@ -0,0 +1,13 @@
+package com.zumr.checkacess_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckacessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckacessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zumr/checkacess_api/CheckacessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zumr.checkacess_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckacessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
